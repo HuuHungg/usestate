f46773fa5f459678c781e64b36d75e0de2afe14e
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-10 21:34:13 +0700

    add file modfile

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
 import {useState} from 'react'
-let total = [100,200,300,700,600]
+let gifts = [
+  '毎日８時間ぐらいITを勉強してる',
+  '毎日２時間ぐらい英語を勉強してる',
+  '毎日２時間ぐらい日本語を勉強してる'
+  
+]
 
 function App() {  
-  let [course, setCourse] = useState(() => {
-    let results = total.reduce((acc,crr) => acc + crr)
-    return results
-  })
+  let [gift, setGift] = useState()
 
-  let handleCourse = () => {
-      setCourse( course + 20)
-  }
-  
-  let [info, setInfo] = useState({
-    名前: "グエンフフン",
-    何歳: "今年は２０歳です",
-    場所: "ベトナムに住んでいる"
-  })
-
-  let handleUpdate = () => {
-    setInfo(
-      {
-        ...info,
-        臭味: "私の臭味はサッカをして本を読んでコーヒーをのみます"
-      }
-    )
+  let handlerSetgid = () => {
+    let ranDom = Math.floor(Math.random() * gifts.length)
+    setGift(gifts[ranDom])
   }
 
   return (
-    <div className="App" style={{padding: 50, color:'red'}}>
-        <h1>{course}</h1>
-        <button onClick={handleCourse}>Increase</button>
-        <h1>{JSON.stringify(info)}</h1>
-        <ul style={{color: 'black'}}>
-          <li>毎日２時間ぐらいITを勉強してる</li>
-          <li>今朝6時に起きました</li>
-          <li>毎日本を読んでいます</li>
-        </ul>
-        <button onClick={handleUpdate}>Update</button>
+    <div className="App" style={{padding: 50, color:'red'}} >
+        <h1>{gift || '毎日勉強して頑張りましょう'}</h1>
+        <button onClick={handlerSetgid} >ボタン</button>
     </div>
   );
 }
 
 export default App;
+  
+
+
+
+
