9ede3fdc9e031fa9db45fdb31fd305d57d82bb12
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-18 13:38:26 +0700

    add file modifile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useReducer } from 'react' 
+import {useState, useReducer, useRef, startTransition } from 'react' 
 // use Reducer giải quyết đượcc cái gì thì useState cũng giải quyết được cái đó
 
 // useState
@@ -11,50 +11,126 @@ import {useState, useReducer } from 'react'
 // 3. Reducer
 // 4. Dispatch
 
-// Init State
-let initState = 0
 
-// Action
-const Up_ACTION = 'up'
-const DOWN_ACTION = 'down'
+let initState = {
+  job: '',
+  jobs: [],
+}
+
+// Actions
+let SET_JOB = 'set_job'
+let ADD_JOB = 'add_job'
+let DELETE_JOB = 'delete_iob'
+
+let setJob = payload => {
+    return {
+      type: SET_JOB,
+      payload
+    }
+}
+
+let addJob = payload => {
+    return {
+        type: ADD_JOB,
+        payload
+    }
+}
+
+let delete_job = payload => {
+    return {
+      type: DELETE_JOB,
+      payload
+    }
+}
 
 // Reducer 
-// Dựa vào action để trả ra state mới
-// Return ra state mới
 let reducer = (state, action) => {
-    console.log('reducer running...')
-    switch(action) {
-        case Up_ACTION:
-            return state + 2
-        case DOWN_ACTION:
-            return state - 2
-        default:
-            throw new Error('Invalid action')
 
-  }
+    console.log('Action: ', action)
+    console.log('Prev state', state)
+
+    let newState 
+
+    switch(action.type) {
+       case SET_JOB:
+          newState = {
+            ...state,
+            job: action.payload
+        }   
+        break
+        case ADD_JOB:
+          newState = {  
+              ...state,
+              jobs: [...state.jobs, action.payload]
+          } 
+        break
+        case DELETE_JOB:
+            const newJobs = [...state.jobs]
+            
+            newJobs.splice(action.payload, 1)
+            newState = {
+                ...state,
+                jobs: newJobs
+            }
+          break
+        default:
+          throw new Error('Invalid action')
+    }
+
+    console.log('New state: ', newState)
+
+    return newState
+
 }
 
 
 
+
+
+
 function App() {  
-  const [count, dispatch] = useReducer(reducer,initState)
+  
+  let [state, dispatch] = useReducer(reducer, initState)
+  const {job, jobs} = state
+
+  let inputRef = useRef()
+
+  let handleSubmit = () => {
+        dispatch(addJob(job))
+        dispatch(setJob(''))
+        
+        inputRef.current.focus()
+  }
 
   return (
-    <div style={{padding: '0 20px'}}>
-         <h1>{count}</h1>
-        <button
-          onClick={() => dispatch(DOWN_ACTION)}
-        >
-          Down
-        </button>
+      <div style={{padding: '20px'}}>
+          <h3>ボタンを直す</h3>
+          <input 
+            ref={inputRef}
+            value={job}
+            placeholder='Enter todo...'
+            onChange={e => {
+                  dispatch(setJob(e.target.value))
+            }}
+          />
+          
+          <button onClick={handleSubmit}>Add</button>
+        <ul>
+            {jobs.map((job, index) => (
+                <li key={index}>
+                  {job} 
+                  <span onClick={() => dispatch(delete_job(index))}>
+                      x
+                  </span>
+                  
+                </li>
+            ))}
+        </ul>
 
-        <button
-          onClick={() => dispatch(Up_ACTION)}
-        >
-          Up
-          </button>
-    </div>
+
+      </div>
   )
+
     
         
 }
